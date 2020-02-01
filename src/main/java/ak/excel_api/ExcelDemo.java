c4a1a0d5681b96c0caabb8f5d27a01a9c8dfb07a
Ayse <makalelr@yahoo.ca>
2020-02-01 05:25:38 -0500

    first commit

--- src/main/java/ak/excel_api/ExcelDemo.java
+++ src/main/java/ak/excel_api/ExcelDemo.java
@@ -2,7 +2,6 @@ package ak.excel_api;
 
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 
@@ -31,59 +30,59 @@ import org.openxmlformats.schemas.drawingml.x2006.chart.CTPlotArea;
 public class ExcelDemo {
 
 	public static void makeChart(String outFile, String[] headers, double[]... dataStream) throws IOException {
-        Workbook wb = new XSSFWorkbook();
-        Sheet sheet = wb.createSheet("chartDemo");
-        
-        int minL = Integer.MAX_VALUE;
-        for (double[] d:dataStream)
-        	minL = Math.min(minL, d.length);
-        	
-        if (minL>25) minL = 25;   // don't show too many-- demo only
+		Workbook wb = new XSSFWorkbook();
+		Sheet sheet = wb.createSheet("chartDemo");
 
-        int nRows = minL;
-        int nCols = dataStream.length;
+		int minL = Integer.MAX_VALUE;
+		for (double[] d:dataStream)
+			minL = Math.min(minL, d.length);
 
-        // column headers
-        Row row = sheet.createRow(0);  
-        for (int colInd = 1; colInd <= nCols; colInd++)
-            row.createCell(colInd)
-            	.setCellValue(headers[colInd-1]);
+		if (minL>25) minL = 25;   // don't show too many-- demo only
 
-        for (int rowInd = 1; rowInd <= nRows; rowInd++) {
-            row = sheet.createRow(rowInd);
-            row.createCell(0).setCellValue(rowInd);
-            for (int colInd = 1; colInd <= nCols; colInd++)
-                row.createCell(colInd)
-                	.setCellValue(dataStream[colInd-1][rowInd]);
-        }
+		int nRows = minL;
+		int nCols = dataStream.length;
 
-        Drawing drawing = sheet.createDrawingPatriarch();
-        Chart chart = drawing.createChart(drawing.createAnchor(0, 0, 0, 0, nCols+2, 4, nCols+15, 22));
-        chart.getOrCreateLegend().setPosition(LegendPosition.TOP_RIGHT);
+		// column headers
+		Row row = sheet.createRow(0);  
+		for (int colInd = 1; colInd <= nCols; colInd++)
+			row.createCell(colInd)
+			.setCellValue(headers[colInd-1]);
 
-        LineChartData data = chart.getChartDataFactory().createLineChartData();
+		for (int rowInd = 1; rowInd <= nRows; rowInd++) {
+			row = sheet.createRow(rowInd);
+			row.createCell(0).setCellValue(rowInd);
+			for (int colInd = 1; colInd <= nCols; colInd++)
+				row.createCell(colInd)
+				.setCellValue(dataStream[colInd-1][rowInd]);
+		}
 
-        ChartAxis xAxis = chart.getChartAxisFactory().createCategoryAxis(AxisPosition.BOTTOM);
-        ValueAxis yAxis = chart.getChartAxisFactory().createValueAxis(AxisPosition.LEFT);
-        ChartDataSource<Number> cds, xs = DataSources.fromNumericCellRange(sheet, new CellRangeAddress(1, nRows, 0, 0));
-        for (int i=1; i<=nCols; i++) {
-        	cds = DataSources.fromNumericCellRange(sheet, new CellRangeAddress(1, nRows, i, i));
-        	data.addSeries(xs, cds).setTitle(headers[i-1]);;
-        }
-        chart.plot(data, xAxis, yAxis);
+		Drawing drawing = sheet.createDrawingPatriarch();
+		Chart chart = drawing.createChart(drawing.createAnchor(0, 0, 0, 0, nCols+2, 4, nCols+15, 22));
+		chart.getOrCreateLegend().setPosition(LegendPosition.TOP_RIGHT);
 
-        CTPlotArea plotArea = ((XSSFChart) chart).getCTChart().getPlotArea();
-        CTMarker marker = CTMarker.Factory.newInstance();
-        marker.setSymbol(CTMarkerStyle.Factory.newInstance());
-        for (CTLineSer ser : plotArea.getLineChartArray()[0].getSerArray()) 
-            ser.setMarker(marker);
+		LineChartData data = chart.getChartDataFactory().createLineChartData();
 
-        FileOutputStream out = new FileOutputStream(outFile);
-        wb.write(out);
-        wb.close();
-        out.close();
-    }
-      
+		ChartAxis xAxis = chart.getChartAxisFactory().createCategoryAxis(AxisPosition.BOTTOM);
+		ValueAxis yAxis = chart.getChartAxisFactory().createValueAxis(AxisPosition.LEFT);
+		ChartDataSource<Number> cds, xs = DataSources.fromNumericCellRange(sheet, new CellRangeAddress(1, nRows, 0, 0));
+		for (int i=1; i<=nCols; i++) {
+			cds = DataSources.fromNumericCellRange(sheet, new CellRangeAddress(1, nRows, i, i));
+			data.addSeries(xs, cds).setTitle(headers[i-1]);;
+		}
+		chart.plot(data, xAxis, yAxis);
+
+		CTPlotArea plotArea = ((XSSFChart) chart).getCTChart().getPlotArea();
+		CTMarker marker = CTMarker.Factory.newInstance();
+		marker.setSymbol(CTMarkerStyle.Factory.newInstance());
+		for (CTLineSer ser : plotArea.getLineChartArray()[0].getSerArray()) 
+			ser.setMarker(marker);
+
+		FileOutputStream out = new FileOutputStream(outFile);
+		wb.write(out);
+		wb.close();
+		out.close();
+	}
+
 	@SuppressWarnings("deprecation")
 	public static double[] readFile(String filePath, String columnHdr) {
 		Sheet sheet = null;
@@ -93,7 +92,7 @@ public class ExcelDemo {
 			// TODO log
 			e.printStackTrace();
 		}
-		
+
 		Row headers = sheet.getRow(0);
 		Cell theColmn = null;
 		for (Cell c:headers)
@@ -105,37 +104,12 @@ public class ExcelDemo {
 		if (!sheet.getRow(1).getCell(colIndex).getCellTypeEnum()
 				.equals(CellType.NUMERIC)) 
 			throw new IllegalArgumentException("Column "+columnHdr+" not numeric");
-		
+
 		double[] columnEntries  = new double[sheet.getPhysicalNumberOfRows()];
 		int i = -1;
 		for (Row row : sheet)
 			if (i++>-1)
 				columnEntries[i]=row.getCell(colIndex).getNumericCellValue();	
-		return columnEntries;
-	}
-
-	@SuppressWarnings("deprecation")
-	public static double[] readFileZ(String filePath, String columnHdr) throws FileNotFoundException, IOException {
-		Workbook wb= new XSSFWorkbook(new FileInputStream(new File(filePath)));
-		Sheet sheet = wb.getSheetAt(0);
-		wb.close();
-		Row headers = sheet.getRow(0);
-		Cell theColmn = null;
-		for (Cell c:headers)
-			if (c.getStringCellValue().trim().equalsIgnoreCase(columnHdr))
-				theColmn = c;
-
-		if (theColmn==null) return null;
-		final int colIndex = theColmn.getColumnIndex();
-		if (!sheet.getRow(1).getCell(colIndex).getCellTypeEnum()
-				.equals(CellType.NUMERIC)) 
-			return null;
-		
-		double[] columnEntries  = new double[sheet.getPhysicalNumberOfRows()];
-		int i = -1;
-		for (Row row : sheet)
-			if (i++>-1)  // parallel array-row indices
-				columnEntries  [i]=row.getCell(colIndex).getNumericCellValue();	
 		return columnEntries;
 	}
 
@@ -150,5 +124,5 @@ public class ExcelDemo {
 		String out = "lineChart_"+System.currentTimeMillis()+".xlsx";
 		makeChart(out, columns, c1, c2, c3);
 	}
-	
+
 }
